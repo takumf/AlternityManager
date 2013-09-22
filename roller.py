44f535815649ed11dc013d648594cfb2f9bc0b65
squirrelsama <sentience0@hotmail.com>
2013-09-22 17:48:01 -0600

    can now roll vs abilities

--- roller.py
+++ roller.py
@@ -54,12 +54,10 @@ def rollReport(tn, results):
 
 def rollVersusStat(character, stat, situation=0, *stipulations):
     def _abilityCheck():
-        return 0
+        return rollReport(manager.skillTnCalc(character.get(stat)), rollPenalty(int(situation)))
     def _otherRoll():
         return 0
     def _skillCheck():
-        if not numericTxt(situation):
-            return "Bad situation modifier: %s"%(situation)
         return rollReport(manager.skillTargetNumbers(character, 
                                                      stat),
                           rollPenalty(int(situation)))
@@ -69,6 +67,8 @@ def rollVersusStat(character, stat, situation=0, *stipulations):
         return _abilityCheck()
     if callable(character[stat]):
         return character[stat](_otherRoll())
+    if not numericTxt(situation):
+        return "Bad situation modifier: %s"%(situation)
     return _skillCheck()
 
 def annotatedRoll(value, arithmetic, diceNotation):
