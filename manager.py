d106430fe3ded9a7efa8e9d4343784deb167f150
Anthony "Ishpeck" Tedjamulia <ishpeck@ishpeck.net>
2013-09-23 10:31:09 -0600

    Broad skills per intelligence.

--- manager.py
+++ manager.py
@@ -23,20 +23,7 @@ def freeSkillPointsFor(character):
     return (character.int-1)*5
 
 def broadSkillsAtCharacterCreationFor(character):
-    return { 4:2,
-             5:2,
-             6:3,
-             7:3,
-             8:4,
-             9:4,
-             10:5,
-             11:5,
-             12:6,
-             13:6,
-             14:7,
-             15:7,
-             16:8,
-        }[character.int]
+    return halfish(character.int)
 
 def halfish(value):
     return (value-(value%2))/2
